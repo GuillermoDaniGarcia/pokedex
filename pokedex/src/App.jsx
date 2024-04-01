e17dc526d6b1ef175eeb00d8efceb461d250e47c
GuillermoDaniGarcia <163834885+GuillermoDaniGarcia@users.noreply.github.com>
2024-04-01 10:35:26 +0200

    empezamos filtro de tipos

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-const-assign */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -5,6 +7,14 @@ import "./App.css";
 function App() {
   // Creamos un estado para poder renderizar el nombre de los pokemons
   const [pokemons, setPokemons] = useState([]);
+
+  // Creamos estado para el filtro de los botones
+  const [filtro, setFiltro] = useState([]);
+
+  // Creamos una función que nos guarde el tipo
+  function Guardado(filtro) {
+    setFiltro(filtro);
+  }
 
   // Creamos una petición a la API para poder obtener los pokemons
   useEffect(() => {
@@ -16,12 +26,12 @@ function App() {
       const listaPokemons = await response.json();
       const { results } = listaPokemons;
 
-      // Creamos una variable para la petición a la API de las imagenes
+      // Creamos una variable para la petición a la API
       const newPokemons = results.map(async (pokemon) => {
         const response = await fetch(pokemon.url);
         const poke = await response.json();
 
-        // Accedemos a las propiedades de la API para poder renderizar las imagenes
+        // Accedemos a las propiedades de la API para poder renderizar los datos
         return {
           id: poke.id,
           name: poke.name,
@@ -38,6 +48,13 @@ function App() {
     getPokemons();
   }, []);
 
+  // Creamos un useEffect para realizar un filtro de la array de pokemons, que pille el tipo que queramos
+  useEffect(() => {
+    if (filtro != null) {
+      pokemons.filter((types) => (types.type.name = filtro));
+    }
+  }, [filtro]);
+
   return (
     <div className="App">
       <header>
@@ -45,97 +62,211 @@ function App() {
           <img src="./img/image.png" alt="Logo Pokédex"></img>
           <ul className="nav-list">
             <li className="nav-item">
-              <button className="btn btn-header" id="ver-todos">
+              <button
+                className="btn btn-header"
+                id="ver-todos"
+                onClick={() => {
+                  Guardado();
+                }}
+              >
                 Ver todos
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header normal" id="normal">
+              <button
+                className="btn btn-header normal"
+                id="normal"
+                onClick={() => {
+                  Guardado("normal");
+                }}
+              >
                 Normal
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header fire" id="fire">
+              <button
+                className="btn btn-header fire"
+                id="fire"
+                onClick={() => {
+                  Guardado("fire");
+                }}
+              >
                 Fire
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header water" id="water">
+              <button
+                className="btn btn-header water"
+                id="water"
+                onClick={() => {
+                  Guardado("water");
+                }}
+              >
                 Water
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header grass" id="grass">
+              <button
+                className="btn btn-header grass"
+                id="grass"
+                onClick={() => {
+                  Guardado("grass");
+                }}
+              >
                 Grass
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header electric" id="electric">
+              <button
+                className="btn btn-header electric"
+                id="electric"
+                onClick={() => {
+                  Guardado("electric");
+                }}
+              >
                 Electric
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header ice" id="ice">
+              <button
+                className="btn btn-header ice"
+                id="ice"
+                onClick={() => {
+                  Guardado("ice");
+                }}
+              >
                 Ice
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header fighting" id="fighting">
+              <button
+                className="btn btn-header fighting"
+                id="fighting"
+                onClick={() => {
+                  Guardado("fighting");
+                }}
+              >
                 Fighting
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header poison" id="poison">
+              <button
+                className="btn btn-header poison"
+                id="poison"
+                onClick={() => {
+                  Guardado("poison");
+                }}
+              >
                 Poison
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header ground" id="ground">
+              <button
+                className="btn btn-header ground"
+                id="ground"
+                onClick={() => {
+                  Guardado("ground");
+                }}
+              >
                 Ground
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header flying" id="flying">
+              <button
+                className="btn btn-header flying"
+                id="flying"
+                onClick={() => {
+                  Guardado("flying");
+                }}
+              >
                 Flying
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header psychic" id="psychic">
+              <button
+                className="btn btn-header psychic"
+                id="psychic"
+                onClick={() => {
+                  Guardado("psychic");
+                }}
+              >
                 Psychic
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header bug" id="bug">
+              <button
+                className="btn btn-header bug"
+                id="bug"
+                onClick={() => {
+                  Guardado("bug");
+                }}
+              >
                 Bug
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header rock" id="rock">
+              <button
+                className="btn btn-header rock"
+                id="rock"
+                onClick={() => {
+                  Guardado("rock");
+                }}
+              >
                 Rock
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header ghost" id="ghost">
+              <button
+                className="btn btn-header ghost"
+                id="ghost"
+                onClick={() => {
+                  Guardado("ghost");
+                }}
+              >
                 Ghost
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header dark" id="dark">
+              <button
+                className="btn btn-header dark"
+                id="dark"
+                onClick={() => {
+                  Guardado("dark");
+                }}
+              >
                 Dark
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header dragon" id="dragon">
+              <button
+                className="btn btn-header dragon"
+                id="dragon"
+                onClick={() => {
+                  Guardado("dragon");
+                }}
+              >
                 Dragon
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header steel" id="steel">
+              <button
+                className="btn btn-header steel"
+                id="steel"
+                onClick={() => {
+                  Guardado("steel");
+                }}
+              >
                 Steel
               </button>
             </li>
             <li className="nav-item">
-              <button className="btn btn-header fairy" id="fairy">
+              <button
+                className="btn btn-header fairy"
+                id="fairy"
+                onClick={() => {
+                  Guardado("fairy");
+                }}
+              >
                 Fairy
               </button>
             </li>
